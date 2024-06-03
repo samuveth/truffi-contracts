da08a61158018594e5500f5efb4511230e2705e4
Sam <samuv.eth@gmail.com>
2024-06-03 10:58:32 +0700

    fixes

--- contracts/Pools.sol
+++ contracts/Pools.sol
@@ -5,14 +5,16 @@ import "./lib/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-uint constant decimals = 9;
-uint constant slots = 50;
+uint256 constant decimals = 9;
+uint256 constant slots = 50;
+uint256 constant reveal = 2;
 
 struct SInscription { uint256 seed; uint256 extra; address creator; }
-struct SPool { uint256 level; uint256 amount; uint256 startedAt; address addr; address owner; }
-struct SPermit { address addr; address owner; uint256 createdAt; }
-struct SSettings { uint256 permitCooldown; uint256 permitExpiration; uint256 exchangeFee; uint256 switchFee; uint256 refreshFee; uint256 createFee; }
+struct SPool { uint256 level; uint256 amount; uint256 createdAt; address addr; address owner; uint256 index; }
+struct SPermit { address addr; address owner; uint256 createdAt; bool allowExchanger; uint256 seed; uint256 range; }
+struct SSettings { uint256 permitCooldown; uint256 permitExpiration; uint256 exchangeFee; uint256 switchFee; uint256 destroyCooldown; address exchanger; }
 
 interface ITruffi {
   function inscriptionOfOwnerByIndex(address, uint256) external pure returns (SInscription memory);
@@ -25,6 +27,7 @@ library DecimalsLib {
 
 contract Pool {
   using SafeERC20 for IERC20;
+  using Address for address payable;
   address immutable token;
   address immutable manager;
 
@@ -38,17 +41,19 @@ contract Pool {
   receive() external payable {}
 
   function safeTransfer(address to, uint256 amount) external onlyPoolManager returns(bool) { IERC20(token).safeTransfer(to, amount); return true; }
-  function withdrawFee(uint256 amount) external onlyPoolManager returns(bool) { payable(manager).transfer(amount); return true; }
+  function withdrawFee(uint256 amount) external onlyPoolManager returns(bool) { payable(manager).sendValue(amount); return true; }
 }
 
 contract Pools is Ownable, ReentrancyGuard {
   using SafeERC20 for IERC20;
   using DecimalsLib for uint256;
+  using Address for address payable;
+
   address immutable token;
   uint256[5] thresholds = [1275, 16225, 61225, 106225, 151225];
   uint256[5] sizes = [1, 300, 1200, 2100, 3000];
 
-  SSettings public settings = SSettings(3, 30, 5000 * 10 ** 10, 0, 25000 * 10 ** 10, 0);
+  SSettings public settings = SSettings(2, 30, 5000 * 10 ** 10, 0, 24 * 1800, address(0));
 
   mapping(address => address) ownerships;
   mapping(address => SPool) pools;
@@ -63,7 +68,7 @@ contract Pools is Ownable, ReentrancyGuard {
   event OnCreate(SPool);
   event OnDestroy(SPool);
   event OnRefresh(SPool);
-  event OnExchange(uint256 indexed, address indexed, SPool, SInscription);
+  event OnExchange(uint256 indexed, address indexed, uint256, SPool, SInscription);
   event OnSwitch(SPermit);
 
   modifier onlyPoolOwner(address addr) {
@@ -72,9 +77,14 @@ contract Pools is Ownable, ReentrancyGuard {
     _;
   }
 
-  modifier onlyWithValidPermit() {
-    require(ownerships[permits[msg.sender].addr] != address(0), "Do not have valid permit");
-    require(permits[msg.sender].createdAt + settings.permitExpiration >= block.number, "Permit is expired for this pool");
+  modifier onlyWithValidPermit(address owner) {
+    require(owner != address(0), "Wrong Permits owner address");
+    SPermit memory permit = getPermit(owner);
+    require(permit.addr != address(0) && ownerships[permit.addr] != address(0), "Not a valid permit");
+    require(block.number >= permits[owner].createdAt + settings.permitCooldown, "Permit is on cooldown");
+    require(block.number <= permit.createdAt + settings.permitExpiration, "Permit is expired");
+    bool validExchanger = settings.exchanger != address(0) && permit.allowExchanger && msg.sender == settings.exchanger;
+    require(msg.sender == owner || validExchanger , "Not allowed to use this permit");
     _;
   }
 
@@ -82,15 +92,21 @@ contract Pools is Ownable, ReentrancyGuard {
 
   receive() external payable {}
 
-  function changeSettings(SSettings calldata settings_) external onlyOwner { settings = settings_; }
+  function changeSettings(SSettings calldata settings_) external onlyOwner {
+    require(settings_.permitCooldown >= 2 && settings_.permitCooldown <= 10, "Permit cooldown must be from 2 to 10 blocks");
+    require(settings_.permitExpiration >= 15 && settings_.permitExpiration <= 900, "Permit expiration must be from 15 to 900 blocks");
+    require(settings_.exchangeFee >= 0 && settings_.exchangeFee <= 100000 * 10 ** 10, "Exchange fee must be from 0 to 0.001 ETH");
+    require(settings_.switchFee >= 0 && settings_.switchFee <= 100000 * 10 ** 10, "Switch Pool fee must from 0 to 0.001 ETH");
+    require(settings_.destroyCooldown >= 0 && settings_.destroyCooldown <= (24 * 1800), "Destroy Pool cooldown must be from 0 to 43200 blocks");
+    settings = settings_;
+  }
 
   function withdraw() external onlyOwner {
     IERC20(token).safeTransfer(msg.sender, IERC20(token).balanceOf(address(this)));
-    payable(msg.sender).transfer(address(this).balance);
+    payable(msg.sender).sendValue(address(this).balance);
   }
 
-  function createPool(uint256 amount) external payable nonReentrant returns(bool) {
-    require(msg.value == settings.createFee, "Incorrect create eth fee");
+  function createPool(uint256 amount) external nonReentrant returns(bool) {
     require(getLevel(amount) > 0, "Incorrect token amount");
 
     address addr = address(new Pool(token, address(this)));
@@ -98,7 +114,7 @@ contract Pools is Ownable, ReentrancyGuard {
 
     IERC20(token).safeTransferFrom(msg.sender, addr, amount.toDec());
 
-    pools[addr] = SPool(getLevel(amount), amount, block.number, addr, ownerships[addr]);
+    pools[addr] = SPool(getLevel(amount), amount, block.number, addr, ownerships[addr], activePoolCount);
 
     for (uint256 i = 0; i < slots; i++) {
       uint256 size = (sizes[pools[addr].level - 1] + i).toDec();
@@ -113,18 +129,16 @@ contract Pools is Ownable, ReentrancyGuard {
     return true;
   }
 
-  function refreshPool(address addr) external payable onlyPoolOwner(addr) nonReentrant returns (bool) {
-    require(msg.value == settings.refreshFee, "Incorrect refresh eth fee");
-
+  function refreshPool(address addr) external onlyPoolOwner(addr) nonReentrant returns (bool) {
     Pool(payable(addr)).safeTransfer(address(this), pools[addr].amount.toDec());
-    IERC20(token).safeTransfer(addr, pools[addr].amount.toDec() - 1);
-    IERC20(token).safeTransfer(addr, 1);
+    IERC20(token).safeTransfer(addr, pools[addr].amount.toDec());
 
     for (uint256 i = 0; i < slots; i++) {
       uint256 size = (sizes[pools[addr].level - 1] + i).toDec();
       Pool(payable(addr)).safeTransfer(address(this), size);
       IERC20(token).safeTransfer(addr, size);
 		}
+
     emit OnRefresh(pools[addr]);
     return true;
   }
@@ -132,30 +146,31 @@ contract Pools is Ownable, ReentrancyGuard {
   function destroyPool(address addr) external onlyPoolOwner(addr) nonReentrant returns(bool) {
     uint256 balance = IERC20(token).balanceOf(addr);
     require(balance > 0, "Pool does not have any tokens");
+    require(block.number > pools[addr].createdAt + settings.destroyCooldown, "Pool can be destroyed only after the cooldown period has passed");
 
     SPool memory pool = pools[addr];
 
     Pool(payable(addr)).safeTransfer(msg.sender, balance);
-    pools[addr] = SPool(0, 0, 0, pool.addr, pool.owner);
+    pools[addr] = SPool(0, 0, 0, pool.addr, pool.owner, pool.index);
 
     emit OnDestroy(pool);
     reindexPools(addr);
     return true;
   }
 
-  function withdrawPoolFees(address addr) public onlyPoolOwner(addr) returns (bool) {
+  function withdrawPoolFees(address addr) public onlyPoolOwner(addr) nonReentrant returns (bool) {
     uint256 balance = payable(addr).balance;
     require(balance > 0, "Pool does not have any fees");
 
     Pool(payable(addr)).withdrawFee(balance);
-    payable(ownerships[addr]).transfer(fees[addr]);
+    payable(ownerships[addr]).sendValue(fees[addr]);
     fees[addr] = 0;
     return true;
   }
 
-  function exchangeInscription(uint256 amount, uint256 extra) external payable onlyWithValidPermit nonReentrant returns(bool) {
+  function exchangeInscription(address owner, uint256 amount, uint256 extra) external payable onlyWithValidPermit(owner) nonReentrant returns(bool) {
     require(msg.value == settings.exchangeFee, "Incorrect exchange eth fee");
-    SPermit memory permit = permits[msg.sender];
+    SPermit memory permit = getPermit(owner);
     SPool memory pool = pools[permit.addr];
     require(pool.amount != 0, "Inactive pool");
     require(amount >= sizes[pool.level - 1] && amount < (sizes[pool.level - 1] + slots), "Token amount is not in a pools range");
@@ -169,26 +184,28 @@ contract Pools is Ownable, ReentrancyGuard {
     IERC20(token).safeTransferFrom(msg.sender, address(this), amount.toDec());
     Pool(payable(addr)).safeTransfer(msg.sender, amount.toDec());
 
-    IERC20(token).safeTransfer(addr, amount.toDec() - 1);
-    IERC20(token).safeTransfer(addr, 1);
 
+    IERC20(token).safeTransfer(addr, 1);
+    Pool(payable(addr)).safeTransfer(address(this), 1);
+
+    IERC20(token).safeTransfer(addr, amount.toDec());
     Pool(payable(addr)).safeTransfer(address(this), amount.toDec());
     IERC20(token).safeTransfer(addr, amount.toDec());
 
-    payable(addr).transfer(settings.exchangeFee);
+    payable(addr).sendValue(settings.exchangeFee);
     fees[addr] = fees[addr] + settings.exchangeFee;
 
-    emit OnExchange(amount, msg.sender, pool, inscription);
+    emit OnExchange(amount, msg.sender, msg.value, pool, inscription);
     exchangeCount++;
     return true;
   }
 
-  function switchPool() external payable returns (SPermit memory) {
+  function switchPool(bool allowExchanger) external payable returns (SPermit memory) {
     require(permits[msg.sender].createdAt == 0 || permits[msg.sender].createdAt + settings.permitCooldown <= block.number, "Permit can not be changed yet");
     require(msg.value == settings.switchFee, "Incorrect switch eth fee");
-    uint256 index = uint256(keccak256(abi.encodePacked(activePoolCount, block.timestamp, exchangeCount, block.prevrandao))) % activePoolCount;
-    address addr = indexes[index];
-    permits[msg.sender] = SPermit(addr, msg.sender, block.number);
+
+    uint256 seed = uint256(keccak256(abi.encodePacked(activePoolCount, block.timestamp, exchangeCount, block.prevrandao)));
+    permits[msg.sender] = SPermit(address(0), msg.sender, block.number, allowExchanger && settings.exchanger != address(0), seed, activePoolCount);
     emit OnSwitch(permits[msg.sender]);
     return permits[msg.sender];
   }
@@ -199,14 +216,22 @@ contract Pools is Ownable, ReentrancyGuard {
 
   function getPoolFee(address addr) external view returns (uint256) { return fees[addr]; }
 
-  function getPermit(address addr) external view returns (SPermit memory) { return permits[addr]; }
+  function getPermit(address owner) public view returns (SPermit memory) {
+    require(permits[owner].seed != 0, "Permit does not exist");
+    if(block.number <= permits[owner].createdAt + reveal) return permits[owner];
+    SPermit memory permit = permits[owner];
+    uint256 index = uint256(keccak256(abi.encodePacked(permit.seed, blockhash(permit.createdAt + reveal)))) % permit.range;
+    return SPermit(indexes[index], permit.owner, permit.createdAt, permit.allowExchanger, permit.seed, permit.range);
+  }
 
   function reindexPools(address addr) internal returns (bool) {
-    bool offset = false;
-    for(uint256 i = 0; i < activePoolCount; i++){
-      if(indexes[i] == addr) offset = true;
-      if(offset) indexes[i] = indexes[i + 1];
-    }
+    uint256 index = pools[addr].index;
+    address lastPoolsAddr = indexes[activePoolCount - 1];
+
+    pools[lastPoolsAddr].index = index;
+    indexes[index] = pools[lastPoolsAddr].addr;
+    indexes[activePoolCount - 1] = address(0);
+
     activePoolCount--;
     return true;
   }
@@ -229,5 +254,4 @@ contract Pools is Ownable, ReentrancyGuard {
     }
     return 0;
   }
-
 }
